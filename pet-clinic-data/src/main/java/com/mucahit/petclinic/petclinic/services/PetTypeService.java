c76c468d6d453284b47556c06401273eb71ceb31
mucahitsahin <mucahit.sahin@anadoluefes.com>
2021-04-26 11:07:53 +0300

    pet type service map added

---
+++ pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.mucahit.petclinic.petclinic.services;
+
+import com.mucahit.petclinic.petclinic.model.PetType;
+import com.mucahit.petclinic.petclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}

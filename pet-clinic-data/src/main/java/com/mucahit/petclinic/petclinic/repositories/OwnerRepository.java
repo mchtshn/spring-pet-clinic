4ecd4796c1d630bbd721fc29bb9a3120b4f13a59
mucahitsahin <mucahit.sahin@anadoluefes.com>
2021-05-04 13:22:29 +0300

    add spring data jpa OwnerSDJpaService close #12

--- pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/repositories/OwnerRepository.java
@@ -3,5 +3,7 @@ package com.mucahit.petclinic.petclinic.repositories;
 import com.mucahit.petclinic.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<Owner,Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }

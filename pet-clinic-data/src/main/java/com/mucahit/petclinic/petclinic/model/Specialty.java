48aaafe8aac84f576138aaff59820793eb5831c3
mucahitsahin <mucahit.sahin@anadoluefes.com>
2021-05-04 12:42:18 +0300

    convert vets to jpa entities ,closes #9

--- pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/model/Specialty.java
@@ -3,9 +3,16 @@ package com.mucahit.petclinic.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "speicialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 }
--- pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/model/Vet.java
@@ -3,15 +3,17 @@ package com.mucahit.petclinic.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-    private String address;
-    private String city;
-    private String telephone;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",joinColumns = @JoinColumn(name = "vet_id"),inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialties = new HashSet<>();
 }

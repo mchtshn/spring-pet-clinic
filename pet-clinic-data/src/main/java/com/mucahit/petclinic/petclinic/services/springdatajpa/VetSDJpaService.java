61eba62e8e7aa50428af01b0cc837a792c35af02
mucahitsahin <mucahit.sahin@anadoluefes.com>
2021-05-04 14:30:38 +0300

    add spring data jpa Vet close #13

---
+++ pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.mucahit.petclinic.petclinic.services.springdatajpa;
+
+import com.mucahit.petclinic.petclinic.model.Vet;
+import com.mucahit.petclinic.petclinic.repositories.VetRepository;
+import com.mucahit.petclinic.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+}

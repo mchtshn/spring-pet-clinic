17b4c527d56310410f25096e11923566385a4f0d
mucahitsahin <mucahit.sahin@anadoluefes.com>
2021-05-04 14:34:30 +0300

    add spring data jpa Vet close #15

--- pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/com/mucahit/petclinic/petclinic/services/PetTypeService.java
@@ -1,7 +1,6 @@
 package com.mucahit.petclinic.petclinic.services;
 
 import com.mucahit.petclinic.petclinic.model.PetType;
-import com.mucahit.petclinic.petclinic.services.CrudService;
 
 public interface PetTypeService extends CrudService<PetType,Long> {
 }
